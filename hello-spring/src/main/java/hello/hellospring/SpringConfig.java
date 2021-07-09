4dcd0ad1da28f630645de6a43468ad0d98aad776
blogSoul <blo9040@gmail.com>
2021-07-09 13:52:36 +0900

    add JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,23 +1,24 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    // @PersistenceContext <- 원래 context에선 붙여야 하지만 생략
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -27,7 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+        // return new JdbcTemplateMemberRepository(dataSource);
         // return new JdbcMemberRepository(dataSource);
         // return new MemoryMemberRepository();
     }
